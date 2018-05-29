b9ce64ba80ce47749b4ef349fce6f553db319089
Jianshu Li <jshu_li@mit.edu>
2018-05-29 19:43:39 -0400

    Make it Module.

--- scripts/OpenMM_Mission_1.py
+++ scripts/OpenMM_Mission_1.py
@@ -2,16 +2,15 @@ from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
 from sys import stdout
+from Redesign_Science.src import DATA_PATH
+import os
 
 try:
     pdb = PDBFile('input.pdb')
     forcefield = ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
 except:
-    try:
-        pdb = PDBFile('/nfs/blender/data/jshu_li/anaconda3/envs/MD_Simulation/lib/python3.6/site-packages/openmm/examples/input.pdb')
-        forcefield = ForceField('/nfs/blender/data/jshu_li/anaconda3/envs/MD_Simulation/lib/python3.6/site-packages/simtk/openmm/app/data/amber14-all.xml', 'amber14/tip3pfb.xml')
-    except:
-        pdb = PDBFile('/Users/JianshuLi/anaconda3/envs/MD_Simulation/lib/python3.6/site-packages/openmm/examples/input.pdb')
+    pdb = PDBFile(os.path.join(DATA_PATH, '../examples/input.pdb'))
+    forcefield = ForceField(DATA_PATH + '/../../simtk/openmm/app/data/amber14-all.xml', DATA_PATH + '/../../simtk/openmm/app/data/amber14/tip3pfb.xml')
         
 
 system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME,
@@ -20,7 +19,7 @@ integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
 simulation = Simulation(pdb.topology, system, integrator)
 simulation.context.setPositions(pdb.positions)
 simulation.minimizeEnergy()
-simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(PDBReporter(DATA_PATH + '/../examples/output.pdb', 1000))
 simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
                                               potentialEnergy=True, temperature=True))
 simulation.step(10000)
